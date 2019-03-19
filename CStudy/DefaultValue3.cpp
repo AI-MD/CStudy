4543836155c83383a7fef99ba98664f1bdfda5e4
jungwhanvision <k2p2048@gmail.com>
2019-03-19 20:20:37 +0900

    test

--- CStudy/DefaultValue3.cpp
+++ CStudy/DefaultValue3.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
-int BoxVolume(int length, int weight = 1, int height = 1);
+int BoxVolume(int length, int weight, int height);
+int BoxVolume(int length, int width);
+int BoxVolume(int length);
+int BoxVolume();
+
 int main(void) {
 	std::cout << "[3,3,3]" << BoxVolume(3, 3, 3) << std::endl;
+	std::cout << "[5,5,d]" << BoxVolume(5,5) << std::endl;
+	std::cout << "[3,d,d]" << BoxVolume(7) << std::endl;
+	std::cout << "[d,d,d]" << BoxVolume() << std::endl;
 	return 0;
 }
 int BoxVolume(int length, int width, int height) {
 	return length * width * height;
+}
+
+int BoxVolume(int length, int width) {
+	std::cout << "hello" << std::endl;
+	return length * width;
+}
+
+int BoxVolume(int length) {
+	return length;
+}
+int BoxVolume() {
+	return 0; 
 }
---
+++ CStudy/a.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include "a.h"
+void BestComImpl::SimpleFunc(void) {
+	std::cout << "BestComImpl이 정의한 함수" << std::endl;
+
+}
+void ProComImpl::SimpleFunc(void) {
+	std::cout << "ProComImpl이 정의한 함수" << std::endl;
+
+}
